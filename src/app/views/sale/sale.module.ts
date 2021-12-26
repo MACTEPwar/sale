c5e75ed590c701955186f3e007a2afc7a06286bc
MACTEPwar <shebanic95@gmail.com>
2021-12-26 05:40:56 +0200

    делаю окно продажи

--- src/app/views/sale/sale.module.ts
+++ src/app/views/sale/sale.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { SaleRoutingModule } from './sale-router.module';
@@ -7,6 +8,12 @@ import { SaleComponent } from './sale.component';
 
 @NgModule({
   declarations: [SaleComponent],
-  imports: [CommonModule, SaleRoutingModule, InputTextModule, TableModule],
+  imports: [
+    CommonModule,
+    SaleRoutingModule,
+    InputTextModule,
+    TableModule,
+    ButtonModule,
+  ],
 })
 export class SaleModule {}
