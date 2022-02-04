edca07ee8eff97364dfa5ef911125de6b897dc11
k.timchenko <k.timchenko@pc-service.ua>
2022-02-04 16:46:12 +0200

    add button styles

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -14,8 +14,6 @@ import { AuthenticationService } from 'src/app/core/authentication/authenticatio
 })
 export class LoginComponent implements OnInit {
   profileForm: FormGroup = new FormGroup({
-    // username: new FormControl('test', Validators.required),
-    // password: new FormControl('flvbyrf2020!@', Validators.required),
     username: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
     fiscal: new FormControl(null),
